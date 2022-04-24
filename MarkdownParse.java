b560fa49a170a2e4e10ed2458ea63949d3d69b43
chaup15 <c6pham@ucsd.edu>
2022-04-23 23:41:31 -0700

    Error Fixed

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,11 +13,28 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            if (openBracket == -1){
+                break;
+            }
+            int openBracket2 = markdown.indexOf("[", openBracket + 1);
             int closeBracket = markdown.indexOf("]", openBracket);
+            if (closeBracket == -1){
+                break;
+            }
+            if (openBracket2 < closeBracket && openBracket2 != -1){
+                closeBracket = markdown.indexOf("]", closeBracket + 1);
+            }
             int openParen = markdown.indexOf("(", closeBracket);
+            if (openParen == -1){
+                break;
+            }
             int closeParen = markdown.indexOf(")", openParen);
+            if (closeParen == -1){
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            
         }
 
         return toReturn;
